a26f946e844e2c06eb099eebe24238b7a8acbe33
ErnestoGarSa <ernest0_10@hotmail.com>
2022-09-07 20:18:12 -0500

    ADD Quoter/Client/LogIn Forms

---
+++ components/ClientForm.jsx
@@ -0,0 +1,294 @@
+import { useForm } from "react-hook-form";
+
+export default function ClientForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // let result = await fetch("http://Ernesto:kodemia123@opsurvey.ymmmfih.mongodb.net/opsurvey/clients", {
+    //   method:"POST",
+    //   body:JSON.stringify(data)
+    // })
+    // console.log(await result.json())
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="relative z-0 mb-6 w-full group mt-3">
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required=""
+          {...register("firstName", { required: true, minLength: 3 })}
+        />
+        {errors.firstName?.type === "required" && "Name is required"}
+        {errors.firstName?.type === "minLength" && "Name min length is 3"}
+        <label
+          htmlFor="firstName"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          First Name
+        </label>
+      </div>
+      <div className="relative z-0 mb-6 w-full group">
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required=""
+          {...register("lastName", { required: true, minLength: 3 })}
+        />
+        {errors.lastName?.type === "required" && "Last Name is required"}
+        {errors.lastName?.type === "minLength" && "Last Name min length is 3"}
+        <label
+          htmlFor="lastName"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Last Name
+        </label>
+      </div>
+      <div className="relative z-0 mb-6 w-full group">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required=""
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Entered value does not match email format",
+            },
+          })}
+        />
+        {errors.email?.type === "required" && "email is required"}
+        {errors.email && <span role="alert">{errors.email.message}</span>}
+        <label
+          htmlFor="email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          email
+        </label>
+      </div>
+      <div className="relative z-0 mb-6 w-full group">
+        <input
+          type="phone"
+          name="phone"
+          id="phone"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required=""
+          {...register("phone", {
+            required: true,
+            minLength: 10,
+          })}
+        />
+        {errors.phone?.type === "required" && "Phone is required"}
+        {errors.phone?.type === "minLength" && "Phone min lenght is 10"}
+        <label
+          htmlFor="phone"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Phone
+        </label>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="boolean"
+            name="isActive"
+            id="isActive"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("isActive")}
+          />
+          <label
+            htmlFor="isActive"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Is Active
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="businessName"
+            id="businessName"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("businessName", { minLength: 5 })}
+          />
+          {errors.businessName?.type === "minLength" &&
+            "Business name min length is 5"}
+          <label
+            htmlFor="businessName"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Bussines Name
+          </label>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="RFC"
+            id="RFC"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("RFC", {
+              required: true,
+              minLength: 12,
+              maxLength: 13,
+            })}
+          />
+          {errors.RFC?.type === "minLength" && "RFC name min length is 12"}
+          {errors.RFC?.type === "maxLength" && "RFC name min length is 13"}
+          {errors.RFC?.type === "required" && "RFC is required"}
+          <label
+            htmlFor="RFC"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            RFC
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="street"
+            id="street"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("street", { required: true })}
+          />
+          {errors.street?.type === "required" && "Street is required"}
+          <label
+            htmlFor="street"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Street
+          </label>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="number"
+            name="extNumber"
+            id="extNumber"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("extNumber", { required: true })}
+          />
+          {errors.extNumber?.type === "required" &&
+            "Exterior Number is required"}
+          <label
+            htmlFor="extNumber"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Exterior Number
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="number"
+            name="intNumber"
+            id="intNumber"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("intNumber")}
+          />
+          <label
+            htmlFor="intNumber"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Interior number{" "}
+          </label>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="number"
+            name="postalCode"
+            id="postalCode"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("postalCode", { required: true })}
+          />
+          {errors.postalCode?.type === "required" && "Postal Code is required"}
+          <label
+            htmlFor="postalCode"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Postal Code
+          </label>
+        </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="neighborhood"
+            id="neighborhood"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("neighborhood", { required: true })}
+          />
+          {errors.neighborhood?.type === "required" &&
+            "Neighborhood is required"}
+          <label
+            htmlFor="neighborhood"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Neighborhood
+          </label>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-6">
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="state"
+            id="state"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("state", { required: true })}
+          />
+          {errors.state?.type === "required" && "State is required"}
+          <label
+            htmlFor="state"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            State
+          </label>
+        </div>
+      </div>
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
---
+++ components/LogInForm.jsx
@@ -0,0 +1,71 @@
+import { useForm } from "react-hook-form";
+
+export default function LogInForm () {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // let result = await fetch("http://Ernesto:kodemia123@opsurvey.ymmmfih.mongodb.net/opsurvey/quoters", {
+    //   method:"POST",
+    //   body:JSON.stringify(data)
+    // })
+    // console.log(await result.json())
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="relative z-0 mb-6 w-full group">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required=""
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Entered value does not match email format",
+            },
+          })}
+        />
+        {errors.email?.type === "required" && "email is required"}
+        {errors.email && <span role="alert">{errors.email.message}</span>}
+        <label
+          htmlFor="email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >email</label>
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="text"
+            name="password"
+            id="password"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required=""
+            {...register("password", { required: true, minLength: 8 })}
+          />
+          {errors.password?.type === "required" && "password is required"}
+          {errors.password?.type === "minLength" && "password min length is 8"}
+          <label
+          htmlFor="password"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >password</label>
+        </div>
+      </div>
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
