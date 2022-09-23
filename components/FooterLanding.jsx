5e37d0ca6d652ae83717f2647ae2d1a9c12333ee
Bere <bre_kc073@hotmail.com>
2022-09-23 17:46:17 -0500

    change a to link in footer

--- components/FooterLanding.jsx
+++ components/FooterLanding.jsx
@@ -5,17 +5,16 @@ export default function FooterLanding() {
     <footer className="bg-black xs:p-3 md:flex flex-row md:items-center md:justify-between md:p-6">
       <span className="md:pl-36 text-base text-lime-400 sm:text-center">
         <img />
-        <a href="https://www.linkedin.com/" className="hover:underline">
-          Linkedin/OpSurvey
-        </a>
+        <Link href="https://www.linkedin.com/">
+          <a className="hover:underline">Linkedin/OpSurvey</a>
+        </Link>
       </span>
       <span className="xs:p-0 md:pr-36 flex flex-wrap items-center mt-3 text-base text-lime-400">
         <Link href="/">
-        <a className="xs:invisible md:visible mr-4 hover:underline md:mr-6 ">
-          OpSurvey
-        </a>
+          <a className="xs:invisible md:visible mr-4 hover:underline md:mr-6 ">
+            OpSurvey
+          </a>
         </Link>
-
       </span>
     </footer>
   );
