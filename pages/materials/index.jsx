3bb2a0d3bcf133599af9b282d30e468926e97f82
Bere <bre_kc073@hotmail.com>
2022-09-27 00:07:55 -0500

    add clientlist file

--- pages/materials/index.jsx
+++ pages/materials/index.jsx
@@ -1,11 +1,9 @@
 import MaterialList from "../../components/MaterialList";
-import NavDashboard from "../../components/NavDashboard"
 
 export default function Materials() {
-    return (
-        <>
-        <NavDashboard/>
-        <MaterialList/>
-        </>
-    )
+  return (
+    <>
+      <MaterialList />
+    </>
+  );
 }
