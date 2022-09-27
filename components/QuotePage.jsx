6d6c927ff481f443c28edfb0465e0a35d07844d2
Bere <bre_kc073@hotmail.com>
2022-09-27 18:55:42 -0500

    edit padding in format quotepage

--- components/QuotePage.jsx
+++ components/QuotePage.jsx
@@ -1,11 +1,11 @@
 export default function QuotePage() {
   return (
     <>
-      <main className="p-10">
-        <section name="quoterInfo" className="bg-white flex flex-row p-4 gap-4">
+      <main className="text-xs">
+        <section name="quoterInfo" className="bg-white flex flex-row gap-4">
           <div
             name="quoter"
-            className="bg-white border-2 border-black rounded-lg p-4 w-2/3"
+            className="bg-white border-2 border-black rounded w-2/3 p-2"
           >
             <p>businessName</p>
             <p>street extNumber intNumber</p>
@@ -16,15 +16,15 @@ export default function QuotePage() {
           </div>
           <div
             name="date"
-            className="bg-white border-2 border-black rounded-lg p-4 w-1/3 text-center"
+            className="bg-white border-2 border-black rounded p-2 w-1/3 text-center"
           >
             <p>Cotizacion 001</p>
             <p>createdAt</p>
           </div>
         </section>
 
-        <section name="clientInfo" className="bg-white p-4">
-          <div className="bg-white p-4 border-2 border-black rounded-lg">
+        <section name="clientInfo" className="bg-white pt-2">
+          <div className="bg-white p-2 border-2 border-black rounded">
             <p>businessName</p>
             <p>firstName lastName</p>
             <p>street - extNumber - intNumber</p>
@@ -34,8 +34,8 @@ export default function QuotePage() {
           </div>
         </section>
 
-        <section name="recpiesInfo" className="bg-white p-4">
-          <div className="bg-white p-4 pl-4 border-2 border-black rounded-lg">
+        <section name="recpiesInfo" className="bg-white pt-2">
+          <div className="bg-white p-2 border-2 border-black rounded">
             <table className="table-auto w-full">
               <thead className="border-b-2 border-black text-left">
                 <tr>
@@ -61,16 +61,51 @@ export default function QuotePage() {
                   <td className="py-2 px-2 w-2/12">199</td>
                   <td className="py-2 px-2 w-2/12">199</td>
                 </tr>
+                <tr className="">
+                  <td className="py-2 px-2 w-5/12">pintura</td>
+                  <td className="py-2 px-2 w-2/12">litro</td>
+                  <td className="py-2 px-2 w-1/12">1</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                </tr>
+                <tr className="">
+                  <td className="py-2 px-2 w-5/12">pintura</td>
+                  <td className="py-2 px-2 w-2/12">litro</td>
+                  <td className="py-2 px-2 w-1/12">1</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                </tr>
+                <tr className="">
+                  <td className="py-2 px-2 w-5/12">pintura</td>
+                  <td className="py-2 px-2 w-2/12">litro</td>
+                  <td className="py-2 px-2 w-1/12">1</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                </tr>
+                <tr className="">
+                  <td className="py-2 px-2 w-5/12">pintura</td>
+                  <td className="py-2 px-2 w-2/12">litro</td>
+                  <td className="py-2 px-2 w-1/12">1</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                </tr>
+                <tr className="">
+                  <td className="py-2 px-2 w-5/12">pintura</td>
+                  <td className="py-2 px-2 w-2/12">litro</td>
+                  <td className="py-2 px-2 w-1/12">1</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                  <td className="py-2 px-2 w-2/12">199</td>
+                </tr>
               </tbody>
             </table>
           </div>
         </section>
 
-        <section name="totalInfo" className="bg-white flex flex-row p-4 gap-4">
-          <div className="bg-white p-4 pl-4 border-2 border-black rounded-lg w-2/3">
+        <section name="totalInfo" className="bg-white flex flex-row pt-2 gap-4">
+          <div className="bg-white p-2 border-2 border-black rounded w-2/3">
             Observaciones
           </div>
-          <div className="bg-white p-4 pl-4 border-2 border-black rounded-lg w-1/3">
+          <div className="bg-white p-2 border-2 border-black rounded w-1/3">
             <p>Subtotal</p>
             <p>Descuentos</p>
             <p>Recargos</p>
